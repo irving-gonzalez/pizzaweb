3dbc5b3040474f1d680b6bb8b7fb024393009669
irvingagonzalez <irving_a_gonzalez@hotmail.com>
2017-08-03 20:57:31 -0500

    first visual studio commit

---
+++ pizzaApp/pizzaApp/Client/Controllers/mainController.js
@@ -0,0 +1,8 @@
+﻿angular.module("main", []);
+
+
+angular.module('main').controller('mainCtrl', function ($scope,$http) {
+
+    $scope.input1 = "name";
+  
+});
---
+++ pizzaApp/pizzaApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pizzaApp.MvcApplication" Language="C#" %>
